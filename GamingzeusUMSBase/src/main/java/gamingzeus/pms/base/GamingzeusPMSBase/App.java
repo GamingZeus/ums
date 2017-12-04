01f0dee3390a8572fb4c76c5ce907965ada32612
Abhirup Bhabani <abhirup@getfocus.in>
2017-12-04 12:56:25 +0530

    Base File Structure for UMS

---
+++ GamingzeusUMSBase/src/main/java/gamingzeus/pms/base/GamingzeusPMSBase/App.java
@@ -0,0 +1,13 @@
+package gamingzeus.pms.base.GamingzeusPMSBase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GamingzeusUMSWeb/src/main/java/gamingzeus/pms/web/GamingzeusPMSWeb/App.java
@@ -0,0 +1,13 @@
+package gamingzeus.pms.web.GamingzeusPMSWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
